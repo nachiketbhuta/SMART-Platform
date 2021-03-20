0a0f4ea912afa8caf4e8763166ec1881e0b53bdd
Nachiket-PC\Nachiket <nachiketbhuta@gmail.com>
2021-03-20 16:04:19 +0530

    Removed unwanted tests

--- backend/test_main.py
+++ backend/test_main.py
@@ -8,8 +8,3 @@ def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"msg": "Hello World"}
-
-def test_read_item():
-    response = client.get("/items/2")
-    assert response.status_code == 200
-    assert response.json() == {"item_id": 2, "q": None}
