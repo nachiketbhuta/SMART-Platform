e1b09711e2538390ee5273ffe82e15c481914ac3
Neelansh Mathur <53081208+neelansh15@users.noreply.github.com>
2021-03-20 19:01:08 +0530

    Watchlist.vue validation

--- frontend/pages/dashboard.vue
+++ frontend/pages/dashboard.vue
@@ -11,52 +11,16 @@
 
 <script lang="ts">
 import Vue from "vue";
-import Watchlist from "~/components/Watchlist.vue";
 export default Vue.extend({
     layout: "main",
-    components: { Watchlist },
     async asyncData({ store, redirect }) {
         if (store.getters.email == "" || store.getters.email == undefined) {
             redirect(200, "/");
         }
     },
     data: () => ({
-        items: [
-            {
-                text: "INFY",
-                price: 300.85,
-            },
-            {
-                text: "TCS",
-                price: 300.85,
-            },
-            {
-                text: "TATASTEEL",
-                price: 300.85,
-            },
-            {
-                text: "WIPRO",
-                price: 300.85,
-            },
-        ],
+      
     }),
-   
-    //  mounted() {
-    //      //Call tickrate more for this fix: https://nuxtjs.org/docs/2.x/features/nuxt-components/#the-client-only-component
-    //     // this.initClientOnlyComp();
-    //     console.log(this.$refs)
-    // },
-    // methods: {
-    //     initClientOnlyComp(count = 10) {
-    //         this.$nextTick(() => {
-    //             if (this.$refs.myComp) {
-    //                 //...
-    //             } else if (count > 0) {
-    //                 this.initClientOnlyComp(count - 1);
-    //             }
-    //         });
-    //     },
-    // },
     created() {
         this.$store.commit("setPageTitle", "Dashboard");
     },
--- frontend/pages/watchlists.vue
+++ frontend/pages/watchlists.vue
@@ -1,15 +1,79 @@
 <template>
     <v-container>
-        <h1>Watchlists</h1>
+        <h1>Watchlist</h1>
+        {{ valid }}
+        <v-form ref="form" v-model="valid">
+            <v-text-field
+                :rules="rules"
+                placeholder="Add a stock"
+                solo
+                v-model="search"
+            ></v-text-field>
+        </v-form>
+
+        <v-list>
+            <v-list-item v-for="(item, i) in watchlist" :key="i">
+                <v-list-item-content>
+                    <v-list-item-title v-text="item"></v-list-item-title>
+                </v-list-item-content>
+                <v-list-item-action>
+                    <v-row>
+                        <v-btn :to="'/dashboard/' + item" icon>
+                            <v-icon>mdi-open-in-new</v-icon>
+                        </v-btn>
+                        <v-btn color="red" icon>
+                            <v-icon>mdi-close</v-icon>
+                        </v-btn>
+                    </v-row>
+                </v-list-item-action>
+            </v-list-item>
+        </v-list>
     </v-container>
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
+import Vue from "vue";
+import firebase from "firebase";
+
 export default Vue.extend({
-    layout: 'main',
-     created(){
-        this.$store.commit('setPageTitle', 'Watchlists')
-    }
-})
+    layout: "main",
+    data: () => ({
+        watchlist: [] as string[],
+        search: "",
+        valid: false,
+    }),
+    computed: {
+        regex_for_rules(){
+            return new RegExp( this.watchlist.join( "|" ), "i");
+        },
+        rules() {
+            return [
+                (v: string) => !!v || "Value is required",
+                (v: string) => {
+                    const alreadyExists: Boolean = this.regex_for_rules.test(v)
+                    return !alreadyExists || "Already in Watchlist"
+                },
+            ];
+        },
+    },
+    created() {
+        this.$store.commit("setPageTitle", "Watchlist");
+    },
+    mounted() {
+        const db = firebase.firestore();
+        db.collection("users")
+            .doc(this.$store.getters.email)
+            .get()
+            .then((doc) => {
+                if (!doc.exists) {
+                    alert(
+                        "User data not found in the database. Logout and Login again"
+                    );
+                } else {
+                    // @ts-ignore
+                    this.watchlist = doc.data().watchlist as string[];
+                }
+            });
+    },
+});
 </script>
