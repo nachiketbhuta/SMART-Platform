b3d9094b20c123e6ac1fa3a30b3d5f29a58fbd20
Neelansh Mathur <53081208+neelansh15@users.noreply.github.com>
2021-03-20 17:53:43 +0530

    Dashboard and nested route: stock

---
+++ frontend/pages/dashboard.vue
@@ -0,0 +1,66 @@
+<template>
+    <v-container>
+        <nuxt-link to="/">Home</nuxt-link>
+        <br />
+        <v-row id="chartContainer">
+            <v-col>
+               <nuxt-child />
+            </v-col>
+        </v-row>
+
+    </v-container>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import Watchlist from "~/components/Watchlist.vue";
+export default Vue.extend({
+    layout: "main",
+    components: { Watchlist },
+    async asyncData({ store, redirect }) {
+        if (store.getters.email == "" || store.getters.email == undefined) {
+            redirect(200, "/");
+        }
+    },
+    data: () => ({
+        items: [
+            {
+                text: "INFY",
+                price: 300.85,
+            },
+            {
+                text: "TCS",
+                price: 300.85,
+            },
+            {
+                text: "TATASTEEL",
+                price: 300.85,
+            },
+            {
+                text: "WIPRO",
+                price: 300.85,
+            },
+        ],
+    }),
+   
+    //  mounted() {
+    //      //Call tickrate more for this fix: https://nuxtjs.org/docs/2.x/features/nuxt-components/#the-client-only-component
+    //     // this.initClientOnlyComp();
+    //     console.log(this.$refs)
+    // },
+    // methods: {
+    //     initClientOnlyComp(count = 10) {
+    //         this.$nextTick(() => {
+    //             if (this.$refs.myComp) {
+    //                 //...
+    //             } else if (count > 0) {
+    //                 this.initClientOnlyComp(count - 1);
+    //             }
+    //         });
+    //     },
+    // },
+    created() {
+        this.$store.commit("setPageTitle", "Dashboard");
+    },
+});
+</script>
---
+++ frontend/pages/dashboard/_stock.vue
@@ -0,0 +1,101 @@
+<template>
+    <client-only>
+        <div>
+            <v-card color="teal" class="my-3">
+                <v-card-title> {{ stock }} </v-card-title>
+                <v-card-subtitle> {{ price }} </v-card-subtitle>
+            </v-card>
+            <trading-vue
+                :width="chartWidth"
+                :data="tradingVue"
+                :overlays="overlays"
+                :extensions="ext"
+                :legend-buttons="['display']"
+            >
+            </trading-vue>
+
+            <v-row>
+                <v-col>
+                    <h1 class="text-h5">Top Tweets</h1>
+                </v-col>
+                <v-col>
+                    <h1 class="text-h5">Top News</h1>
+                </v-col>
+            </v-row>
+        </div>
+    </client-only>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import sample_data from '@/utils/sample_data.json'
+
+// import SetupIndicator from '~/components/SetupIndicator';
+// Importing manually will also throw `windows is not defined error`
+// Using `components: true` in nuxt.config.js
+// import TradingVue from 'trading-vue-js'
+export default Vue.extend({
+    async asyncData({ params }) {
+        const stock = params.stock;
+        return {
+            stock,
+        };
+    },
+    name: "StockChart",
+    // Importing manually will also throw `windows is not defined error`
+    // Using `components: true` in nuxt.config.js
+    // components: { TradingVue }
+    computed: {
+        ext() {
+            // TODO: For some reason the injections are initially
+            // 'undefined'
+            return Object.values(this.$ChartExtensions || {});
+        },
+        chartWidth() {
+            switch (this.$vuetify.breakpoint.name) {
+                case "xs":
+                    return 450;
+                case "sm":
+                    return 450;
+                case "md":
+                    return 800;
+                case "lg":
+                    return 1000;
+                case "xl":
+                    return 1100;
+            }
+        },
+    },
+    data() {
+        return {
+            price: 967.80,
+            // TODO: For some reason the injections are initially
+            // 'undefined'
+            tradingVue: this.$DataCube
+                ? new this.$DataCube({
+                      chart: {
+                          type: "Candles",
+                          data: sample_data.chart.data
+                        //   data: [
+                        //       [1551128400000, 33, 37.1, 14, 14, 196],
+                        //       [1551132000000, 13.7, 30, 6.6, 30, 206],
+                        //       [1551135600000, 29.9, 33, 21.3, 21.8, 74],
+                        //       [1551139200000, 21.7, 25.9, 18, 24, 140],
+                        //       [1551142800000, 24.1, 24.1, 24, 24.1, 29],
+                        //   ],
+                      },
+                      onchart: [
+                          {
+                              name: "Setups",
+                              type: "Setups",
+                              data: [[1551128400000, 1, 35]],
+                              settings: {},
+                          },
+                      ],
+                  })
+                : {},
+            overlays: [this.$SetupIndicator],
+        };
+    },
+});
+</script>
