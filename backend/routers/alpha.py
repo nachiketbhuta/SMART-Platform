4357abbda6c9d662316ff8507eaeec13e207b8b5
Nachiket-PC\Nachiket <nachiketbhuta@gmail.com>
2021-03-20 19:27:54 +0530

    Added technical indicators api

--- backend/routers/alpha.py
+++ backend/routers/alpha.py
@@ -33,3 +33,27 @@ async def get_fundamental_data(symbol: str, function: str):
 async def get_series_data(symbol: str, function: str):
     response = get_data(function, symbol, True)
     return response
+
+@alpha_vantage_router.get("/search/{keywords}")
+async def get_search_results(keywords: str):
+    url = f'https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords={keywords}&apikey={API_KEY}'
+    response = requests.get(url)
+    return response.json()
+
+@alpha_vantage_router.get("/indicators/{symbol}/{function}/{interval}/{time_period}/{series_type}")
+async def get_sma_ema_rsi_data(symbol: str, function: str, interval: str, time_period: str, series_type: str):
+    url = f'https://www.alphavantage.co/query?function={function}&symbol={symbol}&interval={interval}&time_period={time_period}&series_type={series_type}&apikey={API_KEY}'
+    response = requests.get(url)
+    return response.json()
+
+@alpha_vantage_router.get("/indicators/{symbol}/{function}/{interval}")
+async def get_stoch_obv_data(symbol: str, function: str, interval: str):
+    url = f'https://www.alphavantage.co/query?function={function}&symbol={symbol}&interval={interval}&apikey={API_KEY}'
+    response = requests.get(url)
+    return response.json()
+
+@alpha_vantage_router.get("/indicators/{symbol}/{function}/{interval}/{time_period}")
+async def get_adx_cci_data(symbol: str, function: str, interval: str, time_period: str):
+    url = f'https://www.alphavantage.co/query?function={function}&symbol={symbol}&interval={interval}&time_period={time_period}&apikey={API_KEY}'
+    response = requests.get(url)
+    return response.json()
