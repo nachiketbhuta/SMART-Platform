5f67a61e876dec0fe879ccfcb47c6626bc0daa03
Nachiket-PC\Nachiket <nachiketbhuta@gmail.com>
2021-03-21 09:07:58 +0530

    CORS fix

--- backend/main.py
+++ backend/main.py
@@ -10,7 +10,10 @@ app = FastAPI()
 
 origins = [
     "http://localhost",
-    "https://smart-platform-hacknitr.herokuapp.com"
+    "https://smart-platform-hacknitr.herokuapp.com",
+    "http://smart-platform-hacknitr.herokuapp.com",
+    "http://localhost:8081",
+    "http://localhost:3000",
 ]
 
 app.add_middleware(
